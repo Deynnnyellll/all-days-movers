3d5147ca3ff39f6ee2c316e98425021e488d5a66
SethGripon <setharvinpgripon@iskolarngbayan.pup.edu.ph>
2024-02-23 11:01:46 +0800

    fixed navbar and added another section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//  Toggle Dropdown on About Us
 function toggleDropdown() {
   let dropdown = document.querySelector("#dropdown");
   let dropdownIcon = document.querySelector("#dropdown-icon");
@@ -13,13 +14,39 @@ function toggleDropdown() {
   }
 }
 
-toggleBar = (bar) => {
-  let menuBarToggle = document.querySelector("#menu-bar-toggle")
+//  Toggle MenuBar for Small Screen 
+let menuBarToggle = document.querySelector("#toggle-bars")
+let bar = document.querySelector("#menu-bar")
 
-  menuBarToggle.classList.toggle("max-md:translate-y-[450px]")
+toggleBar = () => {
+  if (menuBarToggle.classList.contains("max-md:-translate-y-[190px]")) {
+    menuBarToggle.classList.remove("max-md:-translate-y-[190px]");
+    menuBarToggle.classList.add("translate-y-[20px]");
+  } else {
+    menuBarToggle.classList.remove("translate-y-[20px]");
+    menuBarToggle.classList.add("max-md:-translate-y-[190px]");
+  }
+
   bar.classList.toggle("fa-times")
 };
 
+// Hiding Navbar while Scroll Down
+var scrollPos = window.scrollY
+const navBar = document.querySelector("#nav-bar")
+
+window.addEventListener('scroll', () => {
+  if (scrollPos < window.scrollY) {
+    navBar.classList.add('-translate-y-[100px]')
+    !dropdown.classList.contains("hidden") && dropdown.classList.add("hidden")
+    menuBarToggle.classList.contains("translate-y-[20px]") && (menuBarToggle.classList.remove("translate-y-[20px]") & menuBarToggle.classList.add("max-md:-translate-y-[190px]"))
+    bar.classList.contains("fa-times") && bar.classList.remove("fa-times")
+  } else {
+    navBar.classList.remove('-translate-y-[100px]')
+  }
+  scrollPos = window.scrollY
+})
+
+// Intersection Observer Animation
 const cards = document.querySelectorAll(".card")
 
 const observer = new IntersectionObserver(entries => {
@@ -28,32 +55,33 @@ const observer = new IntersectionObserver(entries => {
     if (entry.isIntersecting) observer.unobserve(entry.target)
   })
 }, {
-  threshold: 1,
+  rootMargin: "-100px",
 })
 
 cards.forEach((card) => {
   observer.observe(card)
 })
 
+// Smooth Scrolling
 window.smoothScroll = function(target) {
-        var scrollContainer = target;
-        do { //find scroll container
-            scrollContainer = scrollContainer.parentNode;
-            if (!scrollContainer) return;
-            scrollContainer.scrollTop += 1;
-        } while (scrollContainer.scrollTop == 0);
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
 
-        var targetY = 0;
-        do { //find the top of target relatively to the container
-            if (target == scrollContainer) break;
-            targetY += target.offsetTop;
-        } while (target = target.offsetParent);
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
 
-        scroll = function(c, a, b, i) {
-            i++; if (i > 30) return;
-            c.scrollTop = a + (b - a) / 30 * i;
-            setTimeout(function(){ scroll(c, a, b, i); }, 20);
-        }
-        // start scrolling
-        scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 20);
     }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
